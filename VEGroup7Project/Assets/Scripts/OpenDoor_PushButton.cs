95d8493793cfabd9dcf0c34caff61b7e0218a25a
MinjiLee-gloria <gloria.minji.lee@gmail.com>
2020-11-25 20:24:06 +0900

    Push button to open door
    Only Push button door algorithm was implemented

---
+++ VEGroup7Project/Assets/Scripts/OpenDoor_PushButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor_PushButton : MonoBehaviour
+{
+    private bool doorOpened;
+    private bool coroutineAllowed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpened = false;
+        coroutineAllowed = true;
+        PushButton.ButtonPressed += RunCoroutine;
+    }
+
+    private void RunCoroutine()
+    {
+        StartCoroutine("OpenThatDoor");
+    }
+
+    private IEnumerator OpenThatDoor()
+     {
+        coroutineAllowed = false;
+        if(!doorOpened)
+        {
+          
+                transform.position = new Vector3(transform.position.x, transform.position.y+ 4.7f, transform.position.z);
+                yield return new WaitForSeconds(0f);
+      
+            doorOpened = true;
+        }
+        else
+        {
+
+                transform.position = new Vector3(transform.position.x, transform.position.y - 4.7f, transform.position.z);
+                yield return new WaitForSeconds(0f);
+   
+            doorOpened = false;
+        }
+        coroutineAllowed = true;
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        PushButton.ButtonPressed -= RunCoroutine;
+    }
+}
---
+++ VEGroup7Project/Assets/Scripts/PushButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PushButton : MonoBehaviour
+{
+    public static event Action ButtonPressed = delegate { };
+    private bool coroutineAllowed = true;
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine("PushTheButton");
+        }
+    }
+
+    private IEnumerator PushTheButton()
+    {
+        ButtonPressed();
+        coroutineAllowed = false;
+        for (int i = 0; i <= 20; i++)
+        {
+            transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y -0.1f, transform.localScale.z);
+            yield return null;
+        }
+        for (int i = 0; i <= 20; i++)
+        {
+            transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + 0.1f, transform.localScale.z);
+            yield return null;
+        }
+        coroutineAllowed = true;
+    }
+
+}
+
+
+
+
